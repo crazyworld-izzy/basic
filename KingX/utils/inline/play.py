5d9df8d87ee6e239e26466226d2555eba38f7d6f
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2023-10-09 13:03:27 +0530

    Update and rename play (1).py to play.py

--- KingX/utils/inline/play.py
+++ KingX/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from ValenciaXMusic.utils.formatters import time_to_seconds
+from KingX.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
