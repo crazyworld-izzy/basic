c2230ac7e1a1ee5dbc6f7f34b7673cbea1cbe83a
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2023-10-09 14:56:01 +0530

    Update watcher.py

--- KingX/plugins/misc/watcher.py
+++ KingX/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ValenciaXMusic import app
-from ValenciaXMusic.core.call import Hotty
+from KingX import app
+from KingX.core.call import Hotty
 
 welcome = 20
 close = 30
