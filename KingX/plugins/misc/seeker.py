4d8465e93be46e78f50ad5b23fa7927d7e7e965c
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2023-10-09 14:55:14 +0530

    Update seeker.py

--- KingX/plugins/misc/seeker.py
+++ KingX/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from ValenciaXMusic.misc import db
-from ValenciaXMusic.utils.database import get_active_chats, is_music_playing
+from KingX.misc import db
+from KingX.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
