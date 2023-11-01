19e306255b65c602c71a245984c384b5c12eb9d0
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2023-11-01 11:32:06 +0530

    Update thumbnails.py

--- KingX/utils/thumbnails.py
+++ KingX/utils/thumbnails.py
@@ -84,7 +84,7 @@ async def get_thumb(videoid):
             background = enhancer.enhance(0.6)
             image2 = background
                                                                                             
-            circle = Image.open("VenomX/assets/circle.png")
+            circle = Image.open("KingX/assets/circle.png")
 
             # changing circle color
             im = circle
@@ -116,13 +116,13 @@ async def get_thumb(videoid):
             image2.paste(circle, (0,0), mask = circle)
 
             # fonts
-            font1 = ImageFont.truetype('VenomX/assets/font.ttf', 30)
-            font2 = ImageFont.truetype('VenomX/assets/font2.ttf', 70)
-            font3 = ImageFont.truetype('VenomX/assets/font2.ttf', 40)
-            font4 = ImageFont.truetype('VenomX/assets/font2.ttf', 35)
+            font1 = ImageFont.truetype('KingX/assets/font.ttf', 30)
+            font2 = ImageFont.truetype('KingX/assets/font2.ttf', 70)
+            font3 = ImageFont.truetype('KingX/assets/font2.ttf', 40)
+            font4 = ImageFont.truetype('KingX/assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "VENOM x MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((10, 10), "KING x MUSIC", fill="white", font = font1, align ="left") 
             image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
