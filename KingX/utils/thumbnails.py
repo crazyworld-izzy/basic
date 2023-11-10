e7f5b054b714049873d395c29ffba9654138fa44
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2023-11-10 21:01:41 +0530

    thumbnails.py

--- KingX/utils/thumbnails.py
+++ KingX/utils/thumbnails.py
@@ -11,7 +11,7 @@ from PIL import ImageFilter, ImageFont, ImageOps
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from DAXXMUSIC import app
+from KingX import app
 from config import YOUTUBE_IMG_URL
 
 
