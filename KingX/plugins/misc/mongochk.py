b20c26236901010451d0b6c975db752e8a212760
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2024-02-15 12:07:01 +0530

    Update mongochk.py

--- KingX/plugins/misc/mongochk.py
+++ KingX/plugins/misc/mongochk.py
@@ -2,13 +2,13 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pymongo import MongoClient
 import re
-from KingX import app as DAXX
+from KingX import app as Hotty
 
 
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
 
 
-@DAXX.on_message(filters.command("mongochk"))
+@Hotty.on_message(filters.command("mongochk"))
 async def mongo_command(client, message: Message):
     if len(message.command) < 2:
         await message.reply("Please enter your MongoDB URL after the command. Example: /mongochk your_mongodb_url")
