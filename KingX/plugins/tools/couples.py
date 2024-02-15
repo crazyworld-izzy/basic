53529093814342ba07e228068bae98a1102a18ec
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2024-02-15 14:35:43 +0530

    Update couples.py

--- KingX/plugins/tools/couples.py
+++ KingX/plugins/tools/couples.py
@@ -128,7 +128,7 @@ async def ctest(_, message):
 #𝐍ᴇxᴛ 𝐂ᴏᴜᴘʟᴇs 𝐖ɪʟʟ 𝐁ᴇ 𝐒ᴇʟᴇᴄᴛᴇᴅ 𝐎ɴ {tomorrow} !!**
 #"""
  
-  [InlineKeyboardButton( text="+ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ +", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+  [InlineKeyboardButton( text="+ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ +", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")]
 
 
     #        await message.reply_photo(b, caption=TXT)
