2f85884ce4542f80ccc5f7ad8e7078f764ef64a5
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2024-05-24 20:08:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 18000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1001991616368"))
 
 # Get this value from @PiyushXManagerBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6258877205))
